b8a018cb5629dff5d8faf611a9c800f13f1bcefe
Xander <cxander2004@gmail.com>
2023-12-04 13:35:03 +0000

    dockerfile

--- mqtt/start.sh
+++ mqtt/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Start both projects
+cd /mqtt/mqtt-broker && docker-compose up &
 cd /mqtt/discovery && npm start &
 cd /mqtt/Lannootree && npm start &
+
 
 # Keep the container running
 tail -f /dev/null
